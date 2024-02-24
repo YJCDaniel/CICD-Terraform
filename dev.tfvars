e6f1bc0c9cfb3a53857f145d1513a03f6c81ec5b
YJCDaniel <yjcdaniel@hotmail.com>
2024-02-24 12:44:36 +0800

    update-2

---
+++ dev.tfvars
@@ -0,0 +1,4 @@
+bucket_region = "us-east-1"
+bucket_env = "dev"
+bucket_name = "Dan"
+bucket_ran_num = "123123"
--- sns.tf
+++
@@ -1,9 +0,0 @@
-# add this
-local { 
-    topic-name = "Dan-updates-topic"
-}
-
-#new sns topic
-resource "aws_sns_topic" "user_updates" {
-  
-}
